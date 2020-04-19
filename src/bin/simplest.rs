e0707a750d7a3c1841e2f0433f05c20eb7fdc507
Ivan Izaguirre <ivanizag@gmail.com>
2020-04-19 20:26:01 +0200

    Fix rustdoc code

--- src/bin/simplest.rs
+++ src/bin/simplest.rs
@@ -9,7 +9,7 @@ fn main() {
 
     // Load program inline or from a file with:
     //      let code = include_bytes!("XXXX.rom");
-    let code = [0x00, 0x00, 0xc3, 0x00, 0x00]; // NOP, NOP, JP $0000
+    let code = [0x3c, 0xc3, 0x00, 0x00]; // INC A, JP $0000
     let size = code.len();
     for i in 0..size {
         machine.poke(0x0000 + i as u16, code[i]);
@@ -21,5 +21,9 @@ fn main() {
         cpu.execute_instruction(&mut state, &mut machine);
 
         // Examine Machine state to update the hosting device as needed.
+        if state.reg.a() == 0x10 {
+            // Let's stop
+            break;
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@
 //!
 //!    // Load program inline or from a file with:
 //!    //      let code = include_bytes!("XXXX.rom");
-//!    let code = [0x00, 0x00, 0xc3, 0x00, 0x00]; // NOP, NOP, JP $0000
+//!    let code = [0x3c, 0xc3, 0x00, 0x00]; // INC A, JP $0000
 //!    let size = code.len();
 //!    for i in 0..size {
 //!        machine.poke(0x0000 + i as u16, code[i]);
@@ -33,6 +33,10 @@
 //!        cpu.execute_instruction(&mut state, &mut machine);
 //!
 //!        // Examine Machine state to update the hosting device as needed.
+//!        if state.reg.a() == 0x10 {
+//!            // Let's stop
+//!            break;
+//!        }
 //!    }
 //!}
 //! ```
